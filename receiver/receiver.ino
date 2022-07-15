42e2b5a8b44e03d4e10121e150908fe461fb9078
ryanchan8662 <96643993+ryanchan8662@users.noreply.github.com>
2022-07-15 13:48:49 -0400

    Interrupts added
    Click function on analogue joysticks now interrupts rather than being caught on loops

---
+++ receiver/receiver.ino
@@ -0,0 +1,14 @@
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void loop() {
+  
+}
+
+ 
---
+++ transmitter/transmitter.ino
@@ -0,0 +1,63 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define Y_PIN 0
+#define X_PIN 1
+#define CLICK_PIN 2
+
+#define RADIO_1 7
+#define RADIO_2 8
+#define PACKETSIZE 2
+
+RF24 radio(RADIO_1, RADIO_2);
+const byte address[6] = "00001";
+
+char click_down;
+long click_time;
+
+void setup () {
+    // initialise global variables
+    click_down = 0;
+    click_time = 0L;
+
+    Serial.begin(9600); // start debug output
+
+    Serial.println("Serial monitor initialised. 1");
+    radio.begin();
+    radio.openWritingPipe(address);
+    radio.setPALevel(RF24_PA_MIN);
+    radio.stopListening();
+    Serial.println("Radio module initialised. 2");
+
+    pinMode(X_PIN, INPUT);
+    pinMode(Y_PIN, INPUT);
+    pinMode(CLICK_PIN, INPUT);
+    Serial.println("Pin inputs initialised. 3");
+    Serial.println("done.");
+
+    // possibly set up pairing sequence if needed later
+}
+
+void calibrate () {
+    
+}
+
+void loop () {
+
+
+    int * reads = (int*)malloc(PACKETSIZE * sizeof(int));
+    reads[0] = analogRead(X_PIN); reads[1] = analogRead(Y_PIN);
+
+    //Serial.print("Joystick: ("); Serial.print(reads[0]); Serial.print(", "); Serial.print(reads[1]); Serial.println(")");
+
+    attachInterrupt(digitalPinToInterrupt(CLICK_PIN), calibrate, LOW); // interrupt for sync
+
+    radio.write(reads, PACKETSIZE * sizeof(int));
+    delay(100);
+}
