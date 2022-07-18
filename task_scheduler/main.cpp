733b30d35bbe4ede48a257bcd762db05489cd260
ryanchan8662 <96643993+ryanchan8662@users.noreply.github.com>
2022-07-18 13:40:48 -0400

    General changes

--- task_scheduler/main.cpp
+++ task_scheduler/main.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <ctime>
 
 #define POLL_RATE_MS 1000
 #define INITIAL_TASKS 5
@@ -9,9 +9,10 @@
 // 69: bad pointer assignment
 
 struct Task {
-    void * target;
-    int priority;
-    long inactive_time;
+    void (*target)();
+    unsigned char priority; // range of 0-255, where 255 is the lowest
+    unsigned long last_active;
+    unsigned long preferred_rate; // optimal rate of execution, ms
 };
 
 Task * tasks;
@@ -29,18 +30,31 @@ void error (char * message) {
 
 }*/
 
+void dummy1 () {
+    printf("I do nothing.\n");
+    return;
+}
+
 int main () { // place code into loop() when finished
-    printf("Program start - emulating Arduino loop\n");
+    
 
     { // emulate Arduino setup() ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        printf("Emulating Arduino setup()\n");
         tasks = (Task*)malloc(INITIAL_TASKS * sizeof(Task));
-        if (tasks == nullptr) {
-            printf("Bad pointer assignment.\n");
-            return (69);
+        if (tasks == nullptr) { printf("Bad pointer assignment.\n"); return (69); }
+
+        printf("Emulating Arduino task setup\n");
+        for (int i = 0; i < INITIAL_TASKS; i++) { // initialise first task list
+            struct Task * temp_task;
+
+            temp_task->target = dummy1;
+            temp_task->priority = i;
+            temp_task->last_active = std::clock(); // set to initial clock
+            temp_task->preferred_rate = 16U;
         }
     }
 
-    while (1) {
-        
+    while (1) { // emulate Arduino loop() ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        printf("Emulating Arduino loop\n");
     }
 }
