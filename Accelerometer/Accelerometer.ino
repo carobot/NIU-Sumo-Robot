be899ac52c7ffe366b540b57a9c311c3356954ea
a-zhuang <angelazhuang9@gmail.com>
2022-07-30 13:12:25 -0400

    Code for accelerometer

---
+++ Accelerometer/Accelerometer.ino
@@ -0,0 +1,32 @@
+#include <Wire.h>                 // Must include Wire library for I2C
+#include "SparkFun_MMA8452Q.h" 
+
+MMA8452Q accel;
+int xAcceleration;
+int zAcceleration;
+
+const int xThreshold=10;
+const int yThreshold=100;
+const int zThreshold=100;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin();
+  accel.init(SCALE_8G, ODR_800); // Init and customize the FSR and ODR
+
+}
+
+void loop() {
+  accel.read();
+  // put your main code here, to run repeatedly:
+  xAcceleration = accel.x; // Read in raw x-axis acceleration data
+  Serial.print("Acceleration on the x-axis is ");
+  Serial.println(xAcceleration);
+
+  zAcceleration = accel.cz; // Read in calculated z-axis acceleration
+  Serial.print("Acceleration on the z-axis is: ");
+  Serial.print(zAcceleration);
+  Serial.println(" g's");
+
+}
