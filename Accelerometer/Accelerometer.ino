028bbcd6f20ed4414155bcc5254f97295dc383bb
a-zhuang <angelazhuang9@gmail.com>
2022-07-30 13:39:31 -0400

    Add code

--- Accelerometer/Accelerometer.ino
+++ Accelerometer/Accelerometer.ino
@@ -3,6 +3,7 @@
 
 MMA8452Q accel;
 int xAcceleration;
+int yAcceleration;
 int zAcceleration;
 
 const int xThreshold=300;//adjust as needed
@@ -19,14 +20,27 @@ void setup() {
 
 void loop() {
   accel.read();
-  // put your main code here, to run repeatedly:
+  
+  // Read Raw value
+  
   xAcceleration = accel.x; // Read in raw x-axis acceleration data
-  //Serial.print("Acceleration on the x-axis is ");
-  //Serial.println(xAcceleration);
+  /*
+  Serial.print("Acceleration on the x-axis is ");
+  Serial.println(xAcceleration);
+  yAcceleration = accel.y; // Read in raw x-axis acceleration data
+  Serial.print("Acceleration on the y-axis is ");
+  Serial.println(yAcceleration);
+  zAcceleration = accel.z; // Read in raw x-axis acceleration data
+  Serial.print("Acceleration on the z-axis is ");
+  Serial.println(zAcceleration);
+  */
   if (abs(xAcceleration) >xThreshold){
     Serial.println("Hit from x side");
   }
-  zAcceleration = accel.cz; // Read in calculated z-axis acceleration
+  
+  //Read in calculated acceleration in unit of g
+  
+  //zAcceleration = accel.cz; // Read in calculated z-axis acceleration
   //Serial.print("Acceleration on the z-axis is: ");
   //Serial.print(zAcceleration);
   //Serial.println(" g's");
