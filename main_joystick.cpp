7f24ba3cb846cbf3ae28f6ebdd7f06a031925f52
ryanchan8662 <96643993+ryanchan8662@users.noreply.github.com>
2022-08-05 12:36:22 -0400

    Final versions

--- main_joystick.cpp
+++ main_joystick.cpp
@@ -3,7 +3,7 @@
 #include <RF24.h>
 #include <nRF24L01.h>
 
-enum Channels {CHANNEL_A = 72, CHANNEL_B = 32, CHANNEL_C = 64, CHANNEL_D = 69};
+enum Channels {CHANNEL_A = 72, CHANNEL_B = 40, CHANNEL_C = 64, CHANNEL_D = 69};
 enum Pins {DIP_0 = 4, DIP_1 = 5};
 
 int upSwitchA = A5;   // left joystick is A
@@ -11,6 +11,7 @@ int downSwitchA = A3;  // right joystick is B
 
 int upSwitchB = 2;
 int downSwitchB = 3;
+unsigned long clock_counter = 0;
 
 RF24 radio(9, 10); // CE, CSN
 const byte address[6] = "00001";
@@ -21,7 +22,7 @@ void setup() {
     Serial.begin(9600); //try 115200
     radio.begin();
     radio.openWritingPipe(address);
-    radio.setPALevel(RF24_PA_MIN);
+    radio.setPALevel(RF24_PA_LOW);
     radio.stopListening();
     //Serial.println("TEST nRF24L01+ transmitter");
       
@@ -52,12 +53,29 @@ void setup() {
             radio.setChannel(CHANNEL_D);
             break;
     }
-    channel_mode = 0x0;
 
 }
 
 void loop() {
-
+    if (millis() % 1000 == 0) { // check for channel change once every second
+        unsigned char channel_mode = 0x0;
+        channel_mode |= (~digitalRead(DIP_0) & 0x1); channel_mode |= (~digitalRead(DIP_1) & 0x1) << 0X1;
+        Serial.println(channel_mode);
+        switch (channel_mode & 0x3) {
+            case (0x0):
+                radio.setChannel(CHANNEL_A);
+                break;
+            case (0x1):
+                radio.setChannel(CHANNEL_B);
+                break;
+            case (0x2):
+                radio.setChannel(CHANNEL_C);
+                break;
+            case (0x3):
+                radio.setChannel(CHANNEL_D);
+                break;
+        }
+    }
     // convert input values to char bit field for transmission
     register unsigned char output_field = 0x00;
     output_field |= (~digitalRead(upSwitchA) & 0x01) << 0x7; output_field |= (~digitalRead(downSwitchA) & 0x01) << 0x6;
--- main_robot.cpp
+++ main_robot.cpp
@@ -3,21 +3,24 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
+#include <Wire.h>
+#include <FastLED.h>
+
 #include <stdlib.h>
 #include <time.h>
 
-enum Pins {MOTOR_LA = 6, MOTOR_LD = 7, MOTOR_RA = 5, MOTOR_RD = 4, RADIO_1 = 9, RADIO_2 = 10, DIP_0 = 3, DIP_1 = 14, DIP_2 = 8, DIP_3 = 2};
-enum Channels {CHANNEL_A = 72, CHANNEL_B = 32, CHANNEL_C = 64, CHANNEL_D = 69};
+enum Pins {MOTOR_LA = 6, MOTOR_LD = 7, MOTOR_RA = 5, MOTOR_RD = 4, RADIO_1 = 9, RADIO_2 = 10, DIP_0 = 3, DIP_1 = 14, DIP_2 = 8, DIP_3 = 2, LED_DATA = 15};
+enum Channels {CHANNEL_A = 72, CHANNEL_B = 40, CHANNEL_C = 64, CHANNEL_D = 69};
 enum DriveMode {TANK_DRIVE, RAMP_DRIVE, DUAL_AXIS_DRIVE};
 enum Directions {BACKWARDS = 0, FORWARDS = 1};
 enum Speeds {STOP = 0, MIN = 64, MID = 128, MAX = 200, OGREDRIVE = 255};
 
-#define POLL_RATE_MS 16 // unused unless program complexity makes robot responsiveness poor
 #define PACKETSIZE 1
 #define INACTIVE_THRESHOLD 1000 // milliseconds until inactivity (1 second)
 #define TICK_LENGTH 50 // milliseconds per timed tick
 #define TURBO_DELAY 100 // delay for turbo kick in, ticks
-#define SLEEP_DELAY 60000
+#define SLEEP_DELAY 30000
+#define SLOW_PERCENTAGE 50 // percentage of slow when hit
 
 RF24 radio(RADIO_1, RADIO_2);
 const byte address[6] = "00001";
@@ -30,8 +33,8 @@ unsigned char * movement_vectors;
 // 7: undefined ~ 128
 // 6: undefined ~ 64
 // 5: turbo active ~ 32
-// 4: robot hit, slowed ~ 16
-// 3: idle ~ 8
+// 4: robot hit, stunned ~ 16
+// 3: idle, sleep ~ 8
 // 2: memory error ~ 4
 // 1: signal not received ~ 2
 // 0: fatal uncategorised error, exit ~ 1
@@ -41,17 +44,30 @@ unsigned char flags;
 unsigned char input_type; // 0 = RF joystick, 1 = USB controller, 2 = bluetooth
 unsigned char dip_switch;
 
-unsigned long counter;
 unsigned long last_transmission_clock;
-unsigned long test_clock;
 
 unsigned long last_tick;
 unsigned long tick_counter;
 unsigned long forward_start_tick;
 
-unsigned long status;
+unsigned char light_strip;
+CRGB * light_array;
+unsigned char light_brightness;
+
+unsigned char pokemon_mode;
+
+void set_light (char lights, char brightness, CRGB colour) {
+    FastLED.setBrightness(brightness);
+    for (int i = 0; i < 8; i++) {
+        if ((lights & (0x1 << i)) >> i) light_array[i] = colour;
+        else light_array[i] = CRGB::Black;
+    } FastLED.show();
+}
 
 void set_speed (char lefts, char rights, char leftd, char rightd) {
+    if (flags & 0x16 >> 4) { // if slowed, reduce input speed
+        lefts /= SLOW_PERCENTAGE; rights /= SLOW_PERCENTAGE;
+    }
     analogWrite(MOTOR_LA, lefts); digitalWrite(MOTOR_LD, leftd);
 
     analogWrite(MOTOR_RA, rights); digitalWrite(MOTOR_RD, rightd);
@@ -73,28 +89,43 @@ void tick () {
     }
 
     // set LED statuses
-    if (flags & 0x1 || flags & 0x4 >> 2) { // fatal code error, memory or other
+    if (((flags & 0x1) || (flags & 0x4)) >> 2) { // fatal code error, memory or other
 
-    } else if (flags & 0x2 >> 1) { // signal not received
- 
-    } else if (flags & 0x8 >> 3) { // robot is idle
+    } else if ((flags & 0x2) >> 1) { // signal not received
+        if ((tick_counter + 2) % 40 == 0) {
+            set_light(0x01, 50, CRGB::Green);
+        } else if (tick_counter % 40 == 0) {
+            set_light(0x00, 50, CRGB::White);
+        }
+    } else if ((flags & 0x8) >> 3) { // robot is idle
 
-    } else if (flags & 0x16 >> 4) { // robot is hit, slowed
-
-    } else if (flags & 0x32 >> 5) { // turbo active
+    } else if ((flags & 0x16) >> 4) { // robot is hit, stunned
+    } else if ((flags & 0x32) >> 5) { // turbo active
 
     } else { // standard operation, no flags
-
+        switch (pokemon_mode) {
+            case (0x0): // pikachu or dragonite
+                set_light(0xFF, 25, CRGB::Yellow);
+                break;
+            case (0x1): // blastoise
+                set_light(0xFF, 25, CRGB::DarkCyan);
+                break;
+            case (0x2): // snorlaxative
+                set_light(0xFF, 25, CRGB::HotPink);
+                break;
+            case (0x3): // eevee
+                set_light(0xFF, 25, CRGB::Chocolate);
+                break;
+        }
     }
 }
 
 void loop () {
     
     if (radio.available()) { // new packet is available to be processed
-
+        flags &= ~0x02;
         *prev_input = *rf_input;
         radio.read(rf_input, PACKETSIZE);
-        //Serial.println(*rf_input);
         last_transmission_clock = millis(); // update last time updated
 
         if (*prev_input != *rf_input && *rf_input == 0x44) { // if new, updated value backs up, initialise ramp
@@ -125,17 +156,28 @@ void loop () {
             if (millis() - last_transmission_clock >= SLEEP_DELAY) {
                 // if inactive for extended time, enter sleep
 
-                // TODO: LED update before sleep
-                while (!radio.available()) {
-                    Serial.println("Entering low power sleep mode.");
-                    delay(10000);
-                }
+                // breathe to indicate entry into LP
+                for (float i = 0; i < 6.28f; i += 0.05f) {
+                    float brightness_val = (sin(i - 1.57f) + 1.0f) * 128.0f;
+                    set_light(0x01, brightness_val, CRGB::Green);
+                    delay(20);
+                } set_light(0x00, 50, CRGB::White);
 
-            
-            } else {
-                // TODO: LED flash per wait tick
-                Serial.println("No activity from transmitter.");
-                delay(1000);
+                // occassional reminder of LP mode
+                while (!radio.available()) {
+                    Serial.println("Low power sleep mode.");
+                    delay(5000);
+                    for (float i = 0; i < 6.28f; i += 0.25f) {
+                        set_light(0x01, (sin(i - 1.57f) + 1.0f) * 32.0f, CRGB::Green);
+                        delay(20);
+                    } set_light(0x00, 50, CRGB::White);
+                }
+                // breathe white when normal transmission resumed
+                for (float i = 0; i < 6.28f; i += 0.5f) {
+                    float brightness_val = (sin(i - 1.57f) + 1.0f) * 128.0f;
+                    set_light(0x01, brightness_val, CRGB::White);
+                    delay(10);
+                } set_light(0x00, 50, CRGB::White);
             }
 
             
@@ -157,30 +199,31 @@ void loop () {
 int main () {
     // setup sequence    
     init(); // i honestly don't know what this is for but it helps with the serial output
-
-    { // clear buffer
-        unsigned char temp_dump = 0;
-        radio.read(&temp_dump, PACKETSIZE);
-    }
-
-    flags = 0x00;
-    counter = 0uL;
-    test_clock = 0uL;
-
-    last_tick = millis();
-    status = 0x0;
-    tick_counter = 0;
-
-
     Serial.begin(9600);
     Serial.println("Program start.");
+
+    /*{ // clear buffer
+        unsigned char temp_dump = 0;
+        radio.read(&temp_dump, PACKETSIZE);
+    }*/
+
+    flags = 0x00;
+
+    last_tick = millis();
+    tick_counter = 0;
+    light_strip = 0x0;
+    light_brightness = 0;
+
+    Wire.begin();
+    
 
     // initialise global variables
     rf_input = (unsigned char*)calloc(PACKETSIZE, sizeof(unsigned char));
     prev_input = (unsigned char*)calloc(PACKETSIZE, sizeof(unsigned char));
     movement_vectors = (unsigned char*)calloc(4, sizeof(unsigned char));
+    light_array = (CRGB*)malloc(6 * sizeof(CRGB));
 
-    if (rf_input == nullptr || movement_vectors == nullptr || prev_input == nullptr) {
+    if (rf_input == nullptr || movement_vectors == nullptr || prev_input == nullptr || light_array == nullptr) {
         Serial.println("Not enough memory, program will now exit.");
         flags |= 0x05;
         return(1);
@@ -201,7 +244,7 @@ int main () {
 
     radio.begin();
     radio.openReadingPipe(0, address);
-    radio.setPALevel(RF24_PA_MIN);
+    radio.setPALevel(RF24_PA_LOW);
     radio.startListening();
 
     // read DIP switch bit field for channel
@@ -226,10 +269,27 @@ int main () {
         default:
             flags |= 0x1; // something is not supposed to happen, kill code
     }
+
+    // read DIP switch bit field for channel
+    pokemon_mode = 0x0;
+        
+    pokemon_mode |= (~digitalRead(DIP_2) & 0x1); pokemon_mode |= (~digitalRead(DIP_3) & 0x1) << 0X1;
+    Serial.print("Pokemon ");
+    Serial.println(pokemon_mode);
     
     // conserve power, turn off switches after read
     digitalWrite(DIP_0, 0); digitalWrite(DIP_1, 0);
     digitalWrite(DIP_2, 0); digitalWrite(DIP_3, 0);
+
+    FastLED.addLeds<NEOPIXEL, LED_DATA>(light_array, 6);
+    
+    for (int i = 0; i < 8; i++) { // end of start sequence
+        set_light(0x1 << i, 100, CRGB::White);
+        delay(75);
+    } for (int i = 7; i >= 0; i--) { // end of start sequence
+        set_light(0x1 << i, 100, CRGB::White);
+        delay(75);
+    } set_light(0x00, 50, CRGB::White);
 
     while (!(flags & 0x01)) { // while no fatal error has been triggered
         loop();
@@ -240,5 +300,6 @@ int main () {
     Serial.println("Program has exit. Reupload or press the reset button to start again.");
     if (flags & 0x02) Serial.println("Fatal memory error.");
     radio.stopListening();
+    set_light(0x01, 50, CRGB::Red);
     return(1);
 }
