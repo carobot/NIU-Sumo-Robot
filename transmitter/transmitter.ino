a85ffe34b2f8da99c35f31e2f1d052c7c47a0d7e
ryanchan8662 <96643993+ryanchan8662@users.noreply.github.com>
2022-08-24 23:50:52 -0400

    Updated with final code

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,65 +1,90 @@
-// transmitter code must standardise radio communication to 0 = neutral, +/- 512 in cardinal directions
-
 #include <Arduino.h>
 #include <SPI.h>
 #include <RF24.h>
 #include <nRF24L01.h>
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+enum Channels {CHANNEL_A = 72, CHANNEL_B = 40, CHANNEL_C = 64, CHANNEL_D = 69};
+enum Pins {DIP_0 = 4, DIP_1 = 5};
 
-#define Y_PIN 0
-#define X_PIN 1
-#define CLICK_PIN 2
+int upSwitchA = A5;   // left joystick is A
+int downSwitchA = A3;  // right joystick is B
 
-#define RADIO_1 7
-#define RADIO_2 8
-#define PACKETSIZE 2
+int upSwitchB = 2;
+int downSwitchB = 3;
+unsigned long clock_counter = 0;
 
-RF24 radio(RADIO_1, RADIO_2);
+RF24 radio(9, 10); // CE, CSN
 const byte address[6] = "00001";
 
-char click_down;
-long click_time;
+void setup() {
+    // put your setup code here, to run once:
 
-void setup () {
-    // initialise global variables
-    click_down = 0;
-    click_time = 0L;
-
-    Serial.begin(9600); // start debug output
-
-    Serial.println("Serial monitor initialised. 1");
+    Serial.begin(9600); //try 115200
     radio.begin();
     radio.openWritingPipe(address);
-    radio.setPALevel(RF24_PA_MIN);
+    radio.setPALevel(RF24_PA_LOW);
     radio.stopListening();
-    Serial.println("Radio module initialised. 2");
+    //Serial.println("TEST nRF24L01+ transmitter");
+      
+    pinMode(upSwitchA, INPUT_PULLUP);
+    pinMode(downSwitchA, INPUT_PULLUP);
 
-    pinMode(X_PIN, INPUT);
-    pinMode(Y_PIN, INPUT);
-    pinMode(CLICK_PIN, INPUT);
-    Serial.println("Pin inputs initialised. 3");
-    Serial.println("done.");
+    pinMode(upSwitchB, INPUT_PULLUP);
+    pinMode(downSwitchB, INPUT_PULLUP);
 
-    // possibly set up pairing sequence if needed later
+    pinMode(DIP_0, INPUT_PULLUP);
+    pinMode(DIP_1, INPUT_PULLUP);
+
+    // read DIP switch bit field for channel
+    unsigned char channel_mode = 0x0;
+    channel_mode |= (~digitalRead(DIP_0) & 0x1); channel_mode |= (~digitalRead(DIP_1) & 0x1) << 0X1;
+    Serial.println(channel_mode);
+    switch (channel_mode & 0x3) {
+        case (0x0):
+            radio.setChannel(CHANNEL_A);
+            break;
+        case (0x1):
+            radio.setChannel(CHANNEL_B);
+            break;
+        case (0x2):
+            radio.setChannel(CHANNEL_C);
+            break;
+        case (0x3):
+            radio.setChannel(CHANNEL_D);
+            break;
+    }
+
 }
 
-void calibrate () {
-    
-}
+void loop() {
+    if (millis() % 1000 == 0) { // check for channel change once every second
+        unsigned char channel_mode = 0x0;
+        channel_mode |= (~digitalRead(DIP_0) & 0x1); channel_mode |= (~digitalRead(DIP_1) & 0x1) << 0X1;
+        Serial.println(channel_mode);
+        switch (channel_mode & 0x3) {
+            case (0x0):
+                radio.setChannel(CHANNEL_A);
+                break;
+            case (0x1):
+                radio.setChannel(CHANNEL_B);
+                break;
+            case (0x2):
+                radio.setChannel(CHANNEL_C);
+                break;
+            case (0x3):
+                radio.setChannel(CHANNEL_D);
+                break;
+        }
+    }
+    // convert input values to char bit field for transmission
+    register unsigned char output_field = 0x00;
+    output_field |= (~digitalRead(upSwitchA) & 0x01) << 0x7; output_field |= (~digitalRead(downSwitchA) & 0x01) << 0x6;
+    //output_field |= digitalRead(rightSwitchA) << 0x5; output_field |= digitalRead(leftSwitchA) << 0x4;
 
-void loop () {
+    output_field |= (~digitalRead(upSwitchB) & 0x01) << 0x3; output_field |= (~digitalRead(downSwitchB) & 0x01) << 0x2;
+    //output_field |= digitalRead(rightSwitchB) << 0x1; output_field |= digitalRead(leftSwitchB);
 
-
-    int * reads = (int*)malloc(PACKETSIZE * sizeof(int));
-    reads[0] = analogRead(X_PIN); reads[1] = analogRead(Y_PIN);
-
-    //Serial.print("Joystick: ("); Serial.print(reads[0]); Serial.print(", "); Serial.print(reads[1]); Serial.println(")");
-
-    attachInterrupt(digitalPinToInterrupt(CLICK_PIN), calibrate, LOW); // interrupt for sync
-
-    radio.write(reads, PACKETSIZE * sizeof(int));
+    //output_field = ~output_field;
+    radio.write(&output_field, sizeof(unsigned char));
     delay(100);
 }
